b5dfa3160780d12c9a19bac4e9908fcb0a123760
Ubuntu <ubuntu@ip-172-31-46-214.us-west-2.compute.internal>
2021-06-04 11:26:33 +0000

    plots

--- plots.py
+++ plots.py
@@ -20,12 +20,16 @@ def plot_for_id_or_ood(id_or_ood):
     plt.savefig(f'accuracies_{id_or_ood}.png')
     plt.clf()
 
-    # plot weight estimation
-    plt.plot(df.wt_true_pos, df.wt_est_pos)
-    plt.xlabel(r'true weights $\frac{q(y)}{p(y)}$')
-    plt.ylabel(r'estimated weights $\hat{C}_p^{-1}q(\hat{y})$')
-    plt.savefig(f'weights_{id_or_ood}.png')
-    plt.clf()
 
 plot_for_id_or_ood('ood')
 plot_for_id_or_ood('id')
+
+# plot weight estimation
+plt.plot(whole_df[whole_df.id_or_ood_val=='id'].wt_true_pos,  whole_df[whole_df.id_or_ood_val=='id'].wt_true_pos, color='black', linestyle='dashed')
+plt.plot(whole_df[whole_df.id_or_ood_val=='ood'].wt_true_pos, whole_df[whole_df.id_or_ood_val=='ood'].wt_est_pos, label='OOD target domain')
+plt.plot(whole_df[whole_df.id_or_ood_val=='id'].wt_true_pos,  whole_df[whole_df.id_or_ood_val=='id'].wt_est_pos,  label='ID target domain')
+plt.legend()
+plt.xlabel(r'true weights $\frac{q(y)}{p(y)}$')
+plt.ylabel(r'estimated weights $\hat{C}_p^{-1}q(\hat{y})$')
+plt.savefig(f'weights.png', bbox_inches='tight')
+plt.clf()
